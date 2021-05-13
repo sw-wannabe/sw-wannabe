9261d14e1f6c5286ac425b65938af5d16c8bcb02
SeungHyun Lee <shlee4290@naver.com>
2021-05-13 08:44:07 +0000

    edit app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,6 +43,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Server listening on port 3000!'));
 
 module.exports = app;
