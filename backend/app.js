f6ee76c6ab95b6b6b969772b27fde203c0f61924
unknownpgr <unknownpgr@gmail.com>
2021-05-14 01:13:43 +0000

    Update port

--- backend/app.js
+++ backend/app.js
@@ -44,6 +44,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`Server listening on port ${process.env.PORT || 3000}!`));
+app.listen(80, () => console.log(`Server listening on port 80!`));
 
 module.exports = app;
